fc0a69192f96ef48472b79a55f645bbf14b3ac4a
Sushil <chaubeyshushil000@gmail.com>
2026-02-15 19:51:03 +0530

    fixed server file

--- server/main.py
+++ server/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from middlewares.exception_handlers import catch_exception_middleware
-from routes.upload_pdfs import router as upload_router
-from routes.ask_question import router as ask_router
+# from middlewares.exception_handlers import catch_exception_middleware
+# from routes.upload_pdfs import router as upload_router
+# from routes.ask_question import router as ask_router
 
 app = FastAPI(title="Medical Assistant API", description="API for Medical Assistant Application", version="1.0")
 
@@ -16,11 +16,11 @@ app.add_middleware(
 
 
 #Middleware exception handlers
-app.middleware("http")(catch_exception_middleware)
+# app.middleware("http")(catch_exception_middleware)
 
 
-#routers
-# 1. Upload PDFs
-app.include_router(upload_router)
-# 2. Ask Question
-app.include_router(ask_router)
+# #routers
+# # 1. Upload PDFs
+# app.include_router(upload_router)
+# # 2. Ask Question
+# app.include_router(ask_router)
